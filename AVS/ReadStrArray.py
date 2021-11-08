7b4c1db8e938fd2fbe7c7271b9ea326d3d5816b3
Sorocin <sorocyanus@gmail.com>
2021-11-08 20:21:48 +0300

    hw

--- AVS/ReadStrArray.py
+++ AVS/ReadStrArray.py
@@ -3,28 +3,25 @@ from extender import *
 
 def read_str_array(container, strArray):
     array_len = len(strArray)
-    i = 0   # Индекс, задающий текущую строку в массиве
+    i = 0
     figNum = 0
     while i < array_len:
         str = strArray[i]
-        key = int(str)   # преобразование в целое
-        if key == 1: # признак прямоугольника
+        key = int(str)
+        if key == 1:
             i += 1
             movie = Game()
-            i = movie.read_str_array(strArray, i) # чтение прямоугольника с возвратом позиции за ним
-        elif key == 2: # признак треугольника
+            i = movie.read_str_array(strArray, i)
+        elif key == 2:
             i += 1
             movie = Cartoon()
-            i = movie.read_str_array(strArray, i) # чтение треугольника с возвратом позиции за ним
+            i = movie.read_str_array(strArray, i)
         elif key == 3:
             i += 1
             movie = Documentary()
-            i = movie.read_str_array(strArray, i)  # чтение треугольника с возвратом позиции за ним
+            i = movie.read_str_array(strArray, i)
         else:
-            # что-то пошло не так. Должен быть известный признак
-            # Возврат количества прочитанных фигур
             return figNum
-        # Количество пробелами фигур увеличивается на 1
         if i == 0:
             return figNum
         figNum += 1
